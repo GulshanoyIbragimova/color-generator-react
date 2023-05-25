d540d7da508603a4ab3dca6433d5004ab92a7968
Gulshanoy Ibrokhimova <ibragimovaguli62@gmail.com>
2023-05-25 13:41:41 +0500

    init: add layout and functionality

---
+++ src/components/SingleColor.jsx
@@ -0,0 +1,27 @@
+import { toast } from "react-toastify";
+const SingleColor = ({ index, color }) => {
+  const { hex, weight } = color;
+  const saveToClipboard = async () => {
+    if (navigator.clipboard) {
+      try {
+        await navigator.clipboard.writeText(`#${hex}`);
+        toast.success("Color copied to clipboard");
+      } catch {
+        toast.error("Failed to copy to clipboard");
+      }
+    } else {
+      toast.error("clipboard access not available");
+    }
+  };
+  return (
+    <article
+      className={index > 10 ? "color color-light" : "color "}
+      style={{ background: `#${hex}` }}
+      onClick={saveToClipboard}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">#{hex}</p>
+    </article>
+  );
+};
+export default SingleColor;
